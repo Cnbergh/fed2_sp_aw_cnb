2c1113e5d4731ff343fd0330333d1ad9e4d68c84
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-12-10 15:11:50 +0100

    package-lock.json error

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
             exit='hidden'
             className='h2 text-center mb-12'
           >
-            Let`&apos`s <span className='text-accent'>connect.</span>
+            Let's <span className='text-accent'>connect.</span>
           </motion.h2>
           {/* form */}
           <motion.form
@@ -40,7 +40,7 @@ const Contact = () => {
             <textarea placeholder='message' className='textarea'></textarea>
             <button className='btn border-2 rounded-full border-white/50 max-w-[700px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group color-accent'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
-                Let`&apos`s talk
+                Let's talk
               </span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
             </button>
